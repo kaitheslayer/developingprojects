ff3b7b9c69ce380f64577ac071e9d9ca2b019f0e
kaitheslayer <makedotools@gmail.com>
2017-03-11 15:06:32 +0200

    Random card printed

---
+++ Python/Fun/randomcardhtml/4linercard.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+# Simple random card webpage
+
+# Import Random Functions
+import random
+
+# Generate Random Number 
+num = str(random.randrange(0,67))
+
+# Allow HTML output
+print ("Content-type: text/html\n\n")
+
+# Print page with image related to randomnumber
+print ("<html><body style='background-color: black; text-align: center; color: white;'><a href='rcard.py'><img src='c/ (%s).png'></a><p>Click on image to generate new card</p></body></html>" % num)
