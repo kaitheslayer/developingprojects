a2d7b274572a225bba028b1d62f9685c8f4247e6
Fried-Liver <Michael.wade@cedarhouse.co.za>
2017-03-13 19:00:57 +0200

    build 0.03
    + new design layout
    - need to change card design to printed card on lines 46 - 51
    + game rules: if yn == ("n")
    ± figure out how to change output text colour??

--- Python/Fun/black jack.py
+++ Python/Fun/black jack.py
@@ -32,51 +32,50 @@ def printer (tprint):
   return
 
 def imgcard (card1, card2, card1s, card2s):
-  print ("  _________   _________ ")
+  print (" .---------. .---------.")
   print (" |    " + card1s + "    | |    " + card2s + "    | ")
   print (" |         | |         |")
   print (" |    " + card1 + "    | |    " + card2 + "    |")
   print (" |         | |         |")
   print (" |    " + card1s + "    | |    " + card2s + "    | ")
-  print ("  ---------   --------- \n")
+  print (" `---------' `---------' \n")
   return
   
   
 
-print ("\033[1;30;41m ______ _       ___  _____  _   __    ___  ___  _____  _   __")
-print ("\033[1;30;41m | ___ \ |     / _ \/  __ \| | / /   |_  |/ _ \/  __ \| | / /")
-print ("\033[1;30;41m | |_/ / |    / /_\ \ /  \/| |/ /      | / /_\ \ /  \/| |/ / ")
-print ("\033[1;30;41m | ___ \ |    |  _  | |    |    \      | |  _  | |    |    \ ")
-print ("\033[1;30;41m | |_/ / |____| | | | \__/\| |\  \ /\__/ / | | | \__/\| |\  \ ")
-print ("\033[1;30;41m \____/\_____/\_| |_/\____/\_| \_/ \____/\_| |_/\____/\_| \_/")
+print ("\033[1;37;40m .-------. ______ _       ___  _____  _   __    ___  ___  _____  _   __ .-------.")
+print ("\033[1;37;40m | J --. | | ___ \ |     / _ \/  __ \| | / /   |_  |/ _ \/  __ \| | / / | J --. |")
+print ("\033[1;37;40m |   ♠   | | |_/ / |    / /_\ \ /  \/| |/ /      | / /_\ \ /  \/| |/ /  |   ♠   |")
+print ("\033[1;37;40m |   ♠   | | ___ \ |    |  _  | |    |    \      | |  _  | |    |    \  |   ♠   |")
+print ("\033[1;37;40m | '-- J | | |_/ / |____| | | | \__/\| |\  \ /\__/ / | | | \__/\| |\  \ | '-- J |")
+print ("\033[1;37;40m `-------' \____/\_____/\_| |_/\____/\_| \_/ \____/\_| |_/\____/\_| \_/ `-------'\n")
 
 
 
-print ("\033[1;30;45m  _________                                       __________      ")
-print ("\033[1;30;45m |    ♠    |____                                 |    ♠     |____ ")
-print ("\033[1;30;45m |         |♠  /                                 |          |♠  / ")
-print ("\033[1;30;45m |    J    |  /                                  |    J     |  /  ")
-print ("\033[1;30;45m |         | /                                   |          | /   ")
-print ("\033[1;30;45m |    ♠    |/                                    |    ♠     |/    ")
-print ("\033[1;30;45m  ---------                                       ----------      \n")
+print ("         .---------.                                     .---------.     ")
+print ("         |    ♠    |____                                 |    ♠    |____ ")
+print ("         |         |♠  /           CREATED BY:           |         |♠  / ")
+print ("         |    J    |  /                                  |    J    |  /  ")
+print ("         |         | /            kaitheslayer           |         | /   ")
+print ("         |    ♠    |/              Fried-Liver           |    ♠    |/    ")
+print ("         `---------'                                     `---------'      \n")
 
 
 
-actg = input ("\033[1;30;42m Press any key to play:")
+actg = input ("                            Press 'Enter' key to play:")
 
+printer ([''])
 printer (['Loading...', ''])
-
-plname = input("What's your name:")
+printer (['...................................................................................................'])
+plname = input("\033[1;37;40m What's your name:")
 printer (["Welcome " + plname + " Do you know how to play blackjack / 21? "])
-yn = input("Y/N:").lower()
+yn = input("\033[1;37;40m Y/N:").lower()
 
 if yn == ("n"):
-    printer (["Here are the rules:", "", "I am the Dealer and you are a player, your goal is to beat my hand without going over 21"])
-
+  printer (["Here are the rules:", "http://www.bicyclecards.com/how-to-play/blackjack/"])
 
 
 elif yn == ("y"):
-    printer (["\033[1;30;42m Dealer ", "\033[1;30;42m  Shuffling cards... ", "\033[1;30;42m  Your cards are " ])
+    printer (["Dealer ", "Shuffling cards... ", "Your cards are " ])
     randomcard ()
     imgcard(card1=rcard1, card2=rcard2, card1s=rcard1s, card2s=rcard2s)
-   
