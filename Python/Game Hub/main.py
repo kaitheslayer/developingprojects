c863a40aeb641b2fb5abed028e6df7dc0bd4e462
kaitheslayer <makedotools@gmail.com>
2017-03-16 15:50:43 +0200

    Update main.py

--- Python/Game Hub/main.py
+++ Python/Game Hub/main.py
@@ -1,19 +1,35 @@
 import pymysql
 
 
+
 def ghsetup (username, pkey, pswd):
     global ghconnect
     ghconnect = pymysql.connect(host='sweb.ga', port=3306, user='sweb_' + username , passwd= pswd, db='sweb_' + pkey)
+    global loggedin
+    loggedin = "false"
 
-def gamelogin(username, password):
+def ghlogin(username, password):
     global ghconnect
     execu = ghconnect.cursor()
-    execu.execute("SELECT * FROM blackjack WHERE username= " + username + " AND password= " + password)
+    execu.execute("SELECT * FROM blackjack WHERE username='%s' AND password='%s'" % (username, password))
     global user
     user = execu.fetchone()
     return "An error Occured"
 
 
-ghsetup(username="mainapp", pkey="gamehub", pswd="31****")
-gamelogin(username="kai", password="mypas")
+
+
+# ================= EXAMPLES =====================
+
+ghsetup(username="mainapp", pkey="gamehub", pswd="314159")
+
+if loggedin == "false":
+    s = input("Do you have a sweb account? (y/n)")
+    if s == "y":
+        uname = input("Whats Your Username?")
+        ghlogin(username=uname, password=pas)
+        
+    if s == "n":
+        uname = input("Create username")
+
 print (user)
