df4047f084342a6ef433b394145c75352f58a03b
kaitheslayer <makedotools@gmail.com>
2017-03-30 22:57:50 +0200

    Update mathfunctions.py

--- Python/Math/mathfunctions.py
+++ Python/Math/mathfunctions.py
@@ -1,12 +1,13 @@
 # File with the functions which will be used in math script
 
-
+# Number to the power of
 def po (number, pof):
     b = number
     for _ in range(pof - 1):
         b = int(b) * int(number)
     return b
 
+# Factors of a number
 def factors (number):
     current, ao, nums  = 0, 0, []
     while current < number:
@@ -16,6 +17,7 @@ def factors (number):
             nums.append(ao)
     return nums
 
+# Sqare root of number
 def sqroot (number):
     fac, f = factors (number), ''
     for x in fac:
@@ -25,6 +27,35 @@ def sqroot (number):
             f = True
     if f != True:
         return "No Square Root Found"
+    
+    
+# THIS CAN SERIOUSLY BE DONE BETTER WITH CREATING OTHER FUCNTIONS, BUT LEAVING IT HERE FOR NOW...
+def lineareq(numbers):
+    
+    ai = numbers[3]
+    bi = numbers[1] * -1
+    ci = numbers[2] * -1
+    di = numbers[0]
+    
+    # Calculate the Determinent of the inverse
+    
+    de = ai * di - bi * ci
+    
+    # Calculate the final answer, for easy eye viewing
+    
+    xo = ai * numbers[4]
+    xoo = bi * numbers[5]
+    ans1 = xo + xoo
+    
+    xo = ci * numbers[4]
+    xoo = di * numbers[5]
+    ans2 = xo + xoo
+    
+    # Finish Equation
+    ans1 = ans1 / de
+    ans2 = ans2 / de
+    return ans1, ans2
+
 
 
 
