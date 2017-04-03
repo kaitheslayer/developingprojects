3d2c247d07b4ebf09a795b4111df642526358336
kaitheslayer <makedotools@gmail.com>
2017-04-03 16:48:22 +0100

    Added quadratic seq solver

--- Python/Math/mathfunctions.py
+++ Python/Math/mathfunctions.py
@@ -40,21 +40,44 @@ def sqroot (number):
     
 
 # Linear Patern Solver    
-def lseq (ls1, ls2, ls3, ls4):
-    if int(ls2) - int(ls1) == int(ls4) - int(ls3):
-        lsd1 = int(ls2) - int(ls1)  # common difference
-        lsc = int(lsd1) - int(ls1)  # constant e.g. Tn = xn + c
-        lsc = int(lsc) * -1
-        if lsd1 == 1:  # added to change Tn = 1n to Tn = n
-            return("Tn = %sn+" % (lsd1) + ("%s" % (lsc)))
-        elif lsc == 0:  # added to prevent problem where 0 is neither '+' or '-'. So a sequence: 1;2;3;4 -> Tn = n0
-            return("Tn = %sn" % (lsd1))
+def lseq(ls1, ls2, ls3, ls4):
+    if ls2 - ls1 == ls4 - ls3:
+        lsd1 = ls2 - ls1  # common difference
+        lsc = lsd1 - ls1  # constant e.g. Tn = xn + c
+        lsc = lsc * -1
+        # Fixes Bug
+        if lsc > 0:
+            lsc = "+" + str(lsc)
+        if lsc == 0:  # added to prevent problem where 0 is neither '+' or '-'. So a sequence: 1;2;3;4 -> Tn = n0
+            return ("Tn = %sn" % (lsd1))
         else:
-            return("Tn = %sn+" % (lsd1) + ("%s" % (lsc)))
+            return ("Tn = %sn" % (lsd1) + ("%s" % (lsc)))
 
     elif ls2 - ls1 != ls4 - ls3:
-        return("This is not a Linear Equation!")
+        return ("This is not a Linear Equation!")
     
+    
+    
+# Quadratic Sequences
+def qseq (qs1, qs2, qs3, qs4):
+    d1 = qs3 - qs2
+    d2 = qs2 - qs1
+    d3 = qs4 - qs3
+    d4 = qs3 - qs2
+    d5 = d1 - d2
+
+    # checking if 2nd difference is constant
+    if d1 - d2 == d3 - d4:
+        a = d5 / 2  # a = 2nd difference/2
+        b = qs2 - qs1 - 3 * a  # b = (T2 - T1)-3a
+        c = qs1 - a - b  # c = T1-(a+b)
+        return "Tn = %sn² + %sn + %s" % ( a, b, c)
+        # use your factor genorator to add (x+y)(x+y) format
+
+    else:  # this line no longer works: enter: 1; 2; 3; 4
+       return "This is not a quadratic sequence!"
+
+
     
 # THIS CAN SERIOUSLY BE DONE BETTER WITH CREATING OTHER FUCNTIONS, BUT LEAVING IT HERE FOR NOW...
 def lineareq(numbers):
