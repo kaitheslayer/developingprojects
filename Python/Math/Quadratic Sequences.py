dbedc40ef69ddb1f811c71beb2b14fefc5e70b55
kaitheslayer <makedotools@gmail.com>
2017-04-03 16:50:07 +0100

    Added function form

--- Python/Math/Quadratic Sequences.py
+++ Python/Math/Quadratic Sequences.py
@@ -32,3 +32,27 @@ if d1-d2==d3-d4:
 
 else:  # this line no longer works: enter: 1; 2; 3; 4
     print("This is not a quadratic sequence!")
+    
+    
+# In Function Form
+    
+def qseq (qs1, qs2, qs3, qs4):
+    d1 = qs3 - qs2
+    d2 = qs2 - qs1
+    d3 = qs4 - qs3
+    d4 = qs3 - qs2
+    d5 = d1 - d2
+
+    # checking if 2nd difference is constant
+    if d1 - d2 == d3 - d4:
+        a = d5 / 2  # a = 2nd difference/2
+        b = qs2 - qs1 - 3 * a  # b = (T2 - T1)-3a
+        c = qs1 - a - b  # c = T1-(a+b)
+        return "Tn = %sn² + %sn + %s" % ( a, b, c)
+        # use your factor genorator to add (x+y)(x+y) format
+
+    else:  # this line no longer works: enter: 1; 2; 3; 4
+       return "This is not a quadratic sequence!"
+
+# EG
+print (qseq(9,16,25,36))
