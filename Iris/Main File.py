b3aa11729c16d3a77b0a4649098ffbb21ab5fdf5
kaitheslayer <makedotools@gmail.com>
2017-08-14 20:30:06 +0200

    Update Main File.py

--- Iris/Main File.py
+++ Iris/Main File.py
@@ -11,14 +11,24 @@ def tr(x):
         'how are you': 'speak("I do not feel emotions.")',
         'add': 'add(ine)',
         'multiply': 'multiply(ine)',
-        'divide': 'division(ine)',
+        'divide': 'divide(ine)',
         'power': 'po(ine)',
-        'minus': 'minus(ine)'
+        'minus': 'minus(ine)',
+        'weather': 'weather(ine)'
     }[x]
 
 
 def speak(x):
     wincl.Dispatch("SAPI.SpVoice").Speak(x)
+
+def output (group, mode, x):
+    # Group 1 = Mathematical Answer, Group 2 = String Ouput
+    if mode == 0:
+        if group == 1:
+            answer(x)
+            speak ("The answer is " + str(x))
+        if group == 2:
+            speak (x)
 
 
 def answer(x):
@@ -35,13 +45,11 @@ def minus(v):
         c.append(float(_))
     y = len(c)
     a = c[0] - sum(c[1:y])
-    answer(a)
-    speak("The answer is " + str(a))
-
-
+    output(1, 0, a)
 
 def e(x):
     exec(x)
+
 
 def add(x):
     t = 0
@@ -49,9 +57,7 @@ def add(x):
     for _ in c:
         t = float(_) + t
     print (t)
-    answer (t)
-    speak ("The answer is " + str(t))
-
+    output(1, 0, t)
 
 
 def multiply(x):
@@ -60,36 +66,34 @@ def multiply(x):
     for _ in c:
         t = float(_) * t
     print (t)
-    answer (t)
-    speak("The answer is " + str(t))
+    output(1, 0, t)
 
 
-def division(x):
-    t = 1
+def divide(x):
     c = re.findall(r'\b\d+\b', x)
-    t = float(c[0]) / float(c[1])
-    print (t)
-    answer (t)
-    speak("The answer is " + str(t))
+    p = c[0]
+    del(c[0])
+    for _ in c:
+        p = float(p) / float(_)
+    output(1, 0, p)
 
 
 def po(x):
     t = 1
     c = re.findall(r'\b\d+\b', x)
     t = float(c[0]) ** float(c[1])
-    print (t)
-    answer (t)
-    speak("The answer is " + str(t))
+    output(1, 0, t)
 
-def weather (lc):
+
+def weather(lc):
+    b = re.findall(r'(?<!\.\s)\b[A-Z][a-z]*\b', lc)
     owm = pyowm.OWM('ae0bcff60dfaaa49d582c81a517da9c0')
-    location = ' '.join(lc)
+    location = ' '.join(b)
     observation = owm.weather_at_place(location)
     w = observation.get_weather()
-    b = "Weather details: \n Status: %s \n Current Temp: %s °C \n Max Temp: %s °C \n Min Temp: %s °C " % ( w.get_status(), w.get_temperature(unit='celsius')['temp'], w.get_temperature(unit='celsius')['temp_max'],
+    b = "It is currently %s  With a temperature of %s °C \n a max of %s °C  and a min of %s °C " % ( w.get_status(), w.get_temperature(unit='celsius')['temp'], w.get_temperature(unit='celsius')['temp_max'],
     w.get_temperature(unit='celsius')['temp_min'])
-    answer(b)
-    speak(b)
+    output(2, 0, b)
 
 
 
@@ -98,7 +102,7 @@ def weather (lc):
 
 
 
-ine = "hey machine 32 minus 21 minus"
+ine = "what is the weather in London"
 words = ['add', 'subtract', 'divide', 'minus', 'multiply', 'weather', 'how are you', 'weather', 'power', 'answer to universe']
 word_exp = '|'.join(words)
 inputi = re.findall(word_exp, ine, re.M)
